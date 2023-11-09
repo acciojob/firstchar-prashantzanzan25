f396af0e5edd28b55028559d040fd2e428787b8a
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-09 14:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstChar(text) {
   // your code here
+	if (s.length === 0) {
+        return '';
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== ' ') {
+            return s[i];
+        }
+    }
+
+    return '';
+	
 }
 
 // Do not change the code below
